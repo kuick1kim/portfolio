db5d0d05cdd81b44db68d4e8f08b84e5f1999e9f
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-13 18:05:57 +0900

    Add files via upload

---
+++ data/partnersData.js
@@ -0,0 +1,54 @@
+function getPartnersData() {
+    return partners;
+}
+
+var partners = {
+    0: {
+        good: '연애상담',
+        bad: '호구능력자',
+    },
+    1: {
+        good: '인스타',
+        bad: '드립',
+    },
+    2: {
+        good: '팩트폭격기',
+        bad: '연애상담',
+    },
+    3: {
+        good: '리액션',
+        bad: '팩트폭격기',
+    },
+    4: {
+        good: '술무새',
+        bad: '리액션',
+    },
+    5: {
+        good: '바른생활',
+        bad: '술무새',
+    },
+    6: {
+        good: '뒷북',
+        bad: '나댐',
+    },
+    7: {
+        good: '드립',
+        bad: '뒷북',
+    },
+    8: {
+        good: '호구능력자',
+        bad: '바른생활',
+    },
+    9: {
+        good: '어리바리',
+        bad: 'TMT',
+    },
+    10: {
+        good: 'TMT',
+        bad: '인스타',
+    },
+    11: {
+        good: '나댐',
+        bad: '어리바리',
+    },
+};
